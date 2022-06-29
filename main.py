78713bee33da4a49c97bcfa07154768a193c891e
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-06-29 09:40:26 -0300

    main retorna listas ao invez de dict

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from PIL import Image
 def main():
     nomes_arquivos_comprovantes = [f for f in listdir("comprovantes") if isfile(join("comprovantes", f))]
 
-    dict_origem_valor = {}
+    origem_valor = []
 
     for nome_arquivo in nomes_arquivos_comprovantes:
         # Abre o arquivo
         imagem = Image.open(f'./comprovantes/{nome_arquivo}')
         valor = descobrir_valor_do_pix(imagem)
         origem = descobrir_origem_pix(imagem)
-        dict_origem_valor[origem] = valor
+        origem_valor.append({'origem': origem, 'valor': valor})
     
-    return dict_origem_valor
+    return origem_valor
 
 x = main()
 print(x)
